6f7fbb5241046bd7a6beb6729c542f5ebd612aa4
OzanOcak <ozanocak@gmail.com>
2023-02-21 19:51:54 -0500

    initial commit

---
+++ calculator/src/utils/utils.js
@@ -0,0 +1,9 @@
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+});
+export default function formatOperand(operand) {
+  if (operand == null) return;
+  const [integer, decimal] = operand.split(".");
+  if (decimal == null) return INTEGER_FORMATTER.format(integer);
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`;
+}
