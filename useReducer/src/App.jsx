1286e6faaaffa997bbd30d2d61b9128cccd7e1d6
OzanOcak <ozanocak@gmail.com>
2023-03-01 13:15:21 -0500

    reducer

--- useReducer/src/App.jsx
+++ useReducer/src/App.jsx
@@ -1,15 +1,29 @@
 import { useReducer } from "react";
 
 function reducer(state, action) {
-  return { count: state.count + 1 };
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      state;
+  }
 }
 
 export default function App() {
   const [state, dispatch] = useReducer(reducer, { count: 0 });
+  function increment() {
+    dispatch({ type: "increment" });
+  }
+  function decrement() {
+    dispatch({ type: "decrement" });
+  }
   return (
-    <div className="App" style={{ display: "flex" }}>
-      <button onClick={() => dispatch()}>+</button>
+    <div className="App" style={{ display: "flex", gap: "2em" }}>
+      <button onClick={increment}>+</button>
       <p>{state.count}</p>
+      <button onClick={decrement}>-</button>
     </div>
   );
 }
