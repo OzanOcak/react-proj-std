475025297844dd019f488d7eb29d6d9475f76e3c
OzanOcak <ozanocak@gmail.com>
2023-03-18 17:30:47 -0400

    rtk thunk

--- todo-redux/src/features/users/usersSlice.jsx
+++ todo-redux/src/features/users/usersSlice.jsx
@@ -1,15 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-  { id: "0", name: "Dude Lebowski" },
-  { id: "1", name: "Neil Young" },
-  { id: "2", name: "Dave Gray" },
-];
+const USERS_URL = "https://jsonplaceholder.typicode.com/users";
+
+const initialState = [];
+
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  const response = await axios.get(USERS_URL);
+  return response.data;
+});
 
 const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  },
 });
 
 export const selectAllUsers = (state) => state.users;
--- todo-redux/src/main.jsx
+++ todo-redux/src/main.jsx
@@ -4,6 +4,9 @@ import App from "./App";
 import "./index.css";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { fetchUsers } from "./features/users/usersSlice";
+
+store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
