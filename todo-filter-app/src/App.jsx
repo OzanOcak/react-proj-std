181af5be1b703c3ce72c2d95ebc57a7a6e69c969
OzanOcak <ozanocak@gmail.com>
2023-04-11 23:29:12 -0400

    stage 2/stateful jsx

--- todo-filter-app/src/App.jsx
+++ todo-filter-app/src/App.jsx
@@ -1,27 +1,63 @@
+import { useState } from "react";
+
 function App() {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos((currTodos) => {
+      return [
+        ...currTodos,
+        { id: crypto.randomUUID(), text: text, completed: false },
+      ];
+    });
+    setText("");
+  };
+  const handleDelete = (todoId) => {
+    const updatedTodos = (currTodos) => {
+      return currTodos.filter((todo) => todo.id !== todoId);
+    };
+    setTodos(updatedTodos);
+  };
+  function handleCheck(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+
+        return todo;
+      });
+    });
+  }
   return (
     <div className="App">
       <label>Todo App w/ filter</label>
-      <form>
-        <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <button>add</button>
       </form>
       <div>
         <ul>
-          <li>
-            <span>
-              <input type="checkbox" />
-              list item 1
-            </span>
-            <button>delete</button>
-          </li>
-          <li>
-            <span>
-              <input type="checkbox" />
-              list item 2
-            </span>
-            <button>delete</button>
-          </li>
+          {todos.map((todo) => {
+            return (
+              <li key={todo.id}>
+                <span>
+                  <input
+                    type="checkbox"
+                    checked={todo.checked}
+                    onChange={(e) => handleCheck(todo.id, e.target.checked)}
+                  />
+                  {todo.text}
+                </span>
+                <button onClick={() => handleDelete(todo.id)}>delete</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
