0aac153cf3b9a411d985fb2f095218a0e8a9032f
OzanOcak <ozanocak@gmail.com>
2023-04-14 22:47:28 -0400

    stage 3 - list

---
+++ todo-filter-app/src/TodoItem.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function TodoItem({ todo, setTodos }) {
+  const handleDelete = (todoId) => {
+    const updatedTodos = (currTodos) => {
+      return currTodos.filter((todo) => todo.id !== todoId);
+    };
+    setTodos(updatedTodos);
+  };
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+
+        return todo;
+      });
+    });
+  }
+  return (
+    <div>
+      <span>
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+        />
+        {todo.text}
+      </span>
+      <button onClick={() => handleDelete(todo.id)}>delete</button>
+    </div>
+  );
+}
+
+export default TodoItem;
